e75edceb773929cf734625232f0535579c0f0ba7
Jorge Avila <68880188+javila-dev@users.noreply.github.com>
2026-03-04 03:17:31 +0000

    chore: dejar static_files fuera de versionado

--- static_files/js/preventDbClick.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('input:submit').on('click',function(e){
-        if($(this).hasClass('disabled')){
-            e.preventDefault();
-        }
-        $(this).addClass('disabled')
-    })
-  })
